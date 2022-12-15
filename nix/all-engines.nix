prisma/prisma-engines

commit ad193ffaf4416fcf82ff5ce1c214f798c14a50cf

nix: build libquery_engine.node too in packages.prisma-engines  (#3498)
* flake.lock: Update

Flake lock file updates:

• Updated input 'crane':
    'github:ipetkov/crane/c61d98aaea5667607a36bafe5a6fa87fe5bb2c7e' (2022-11-21)
  → 'github:ipetkov/crane/59b31b41a589c0a65e4a1f86b0e5eac68081468b' (2022-12-13)
• Updated input 'crane/flake-compat':
    'github:edolstra/flake-compat/b4a34015c698c7793d592d66adbab377907a2be8' (2022-04-19)
  → 'github:edolstra/flake-compat/009399224d5e398d03b22badca40a37ac85412a1' (2022-11-17)
• Updated input 'flake-parts':
    'github:hercules-ci/flake-parts/d591857e9d7dd9ddbfba0ea02b43b927c3c0f1fa' (2022-11-14)
  → 'github:hercules-ci/flake-parts/8d0e2444ab05f79df93b70e5e497f8c708eb6b9b' (2022-12-07)
• Updated input 'nixpkgs':
    'github:NixOS/nixpkgs/690ffff026b4e635b46f69002c0f4e81c65dfc2e' (2022-11-20)
  → 'github:NixOS/nixpkgs/1710ed1f6f8ceb75cf7d1cf55ee0cc21760e1c7a' (2022-12-13)
• Updated input 'rust-overlay':
    'github:oxalica/rust-overlay/68ab029c93f8f8eed4cf3ce9a89a9fd4504b2d6e' (2022-11-21)
  → 'github:oxalica/rust-overlay/da99b6227d450438d53ba4b0cf64e43ad2e93e58' (2022-12-14)

* nix: build libquery_engine.node too in packages.prisma-engines

We were building all binaries but not the node-api version of the QE.

This PR also simplifies how prisma-engines is built. Dependency caching
with crane does not work in this repo, so we get crane out of the
equation, except for the vendoring of the deps from Cargo.lock. The
prisma-engines derivation becomes a simple, standard mkDerivation based
derivation.

This is a prerequisite for https://github.com/prisma/prisma/issues/16767

nix/all-engines.nix
@@ -1,35 +1,45 @@
 { craneLib, pkgs, ... }:
 
 let
+  srcPath = builtins.path { path = ../.; name = "prisma-engines-workspace-root-path"; };
+  src = pkgs.lib.cleanSourceWith { filter = enginesSourceFilter; src = srcPath; };
+  deps = craneLib.vendorCargoDeps { inherit src; };
+
   enginesSourceFilter = path: type: (builtins.match "\\.pest$" path != null) ||
     (builtins.match "\\.README.md$" path != null) ||
     (builtins.match "^\\.git/HEAD" path != null) ||
     (builtins.match "^\\.git/refs" path != null) ||
     (craneLib.filterCargoSources path type != null);
-  src = pkgs.lib.cleanSourceWith {
-    filter = enginesSourceFilter;
-    src = builtins.path {
-      path = ../.;
-      name = "prisma-engines-workspace-root-path";
-    };
-  };
+in
+{
+  packages.prisma-engines-deps = deps;
+  packages.prisma-engines = pkgs.stdenv.mkDerivation {
+    name = "prisma-engines";
+    inherit src;
 
-  craneArgs = {
-    pname = "prisma-engines";
-    version = "0.1.0";
     buildInputs = [ pkgs.openssl ];
-    cargoExtraArgs = "--workspace --all-features --bins";
     nativeBuildInputs = with pkgs; [
       git # for our build scripts that bake in the git hash
       perl # for openssl-sys
       pkg-config
       protobuf # for tonic
+      cargo
     ];
-    doCheck = false;
-    inherit src;
+
+    buildPhase = ''
+      mkdir .cargo
+      ln -s ${deps}/config.toml .cargo/config.toml
+      cargo build --release --bins
+      cargo build --release -p query-engine-node-api
+    '';
+
+    installPhase = ''
+      mkdir -p $out/bin $out/lib
+      cp target/release/query-engine $out/bin/
+      cp target/release/migration-engine $out/bin/
+      cp target/release/introspection-engine $out/bin/
+      cp target/release/prisma-fmt $out/bin/
+      cp target/release/libquery_engine.so $out/lib/libquery_engine.node
+    '';
   };
-in
-  {
-    packages.prisma-engines-deps = craneLib.buildDepsOnly craneArgs;
-    packages.prisma-engines = craneLib.buildPackage craneArgs;
-  }
+}

commit 69648f648e068bdc3fd98a65cee6a91e0ba60096

nix devShell: use lld to link rust programs
lld is very noticeably faster than gold. This commit makes users of the
nix devShell link with lld transparently: the devShell takes care of
installing it, and making rustc use it (through RUSTFLAGS).

This commit also fixes the out-of-date inputsFrom that broke in
ad193ffaf4416fcf82ff5ce1c214f798c14a50cf.

nix/all-engines.nix
@@ -17,13 +17,13 @@ in
     name = "prisma-engines";
     inherit src;
 
-    buildInputs = [ pkgs.openssl ];
+    buildInputs = [ pkgs.openssl.out ];
     nativeBuildInputs = with pkgs; [
-      git # for our build scripts that bake in the git hash
-      perl # for openssl-sys
-      pkg-config
-      protobuf # for tonic
       cargo
+      git # for our build scripts that bake in the git hash
+      protobuf # for tonic
+      openssl.dev
+      pkg-config
     ];
 
     buildPhase = ''

nix/shell.nix
@@ -1,8 +1,12 @@
 { config, pkgs, ... }:
 
+let
+  devToolchain = pkgs.rustToolchain.default.override { extensions = [ "rust-analyzer" "rust-src" ]; };
+in
 {
   devShells.default = pkgs.mkShell {
-    packages = [ (pkgs.rustToolchain.default.override { extensions = [ "rust-analyzer" "rust-src" ]; }) ];
-    inputsFrom = [ config.packages.prisma-engines-deps ];
+    packages = [ devToolchain pkgs.llvmPackages.bintools ];
+    inputsFrom = [ config.packages.prisma-engines ];
+    shellHook = "export RUSTFLAGS='-C link-arg=-fuse-ld=lld'";
   };
 }