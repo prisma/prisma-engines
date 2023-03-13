prisma/prisma-engines

commit 066eebeebbe9d8704466a094f1994403117cb101

feat(qe): Query validation (I): preparatory refactoring (#3733)

query-engine/core/src/error.rs
@@ -6,6 +6,7 @@ use bigdecimal::BigDecimal;
 use connector::error::ConnectorError;
 use prisma_models::DomainError;
 use thiserror::Error;
+use user_facing_errors::UnknownError;
 
 #[derive(Debug, Error)]
 #[error(
@@ -166,22 +167,33 @@ impl From<CoreError> for user_facing_errors::Error {
                 ..
             })) => user_facing_error.into(),
 
-            CoreError::QueryParserError(query_parser_error)
-            | CoreError::QueryGraphBuilderError(QueryGraphBuilderError::QueryParserError(query_parser_error)) => {
-                let known_error = match query_parser_error.error_kind {
+            // legacy query validation errors. This block should go away
+            CoreError::QueryParserError(QueryParserError::Legacy { path, error_kind })
+            | CoreError::QueryGraphBuilderError(QueryGraphBuilderError::QueryParserError(QueryParserError::Legacy {
+                path,
+                error_kind,
+            })) => {
+                let known_error = match error_kind {
                     QueryParserErrorKind::RequiredValueNotSetError => {
                         user_facing_errors::KnownError::new(user_facing_errors::query_engine::MissingRequiredValue {
-                            path: format!("{}", query_parser_error.path),
+                            path: format!("{}", path),
                         })
                     }
-                    _ => user_facing_errors::KnownError::new(user_facing_errors::query_engine::QueryValidationFailed {
-                        query_validation_error: format!("{}", query_parser_error.error_kind),
-                        query_position: format!("{}", query_parser_error.path),
-                    }),
+                    _ => user_facing_errors::KnownError::new(
+                        user_facing_errors::query_engine::LegacyQueryValidationFailed {
+                            query_validation_error: format!("{}", error_kind),
+                            query_position: format!("{}", path),
+                        },
+                    ),
                 };
 
                 known_error.into()
             }
+
+            CoreError::QueryParserError(QueryParserError::Structured(se))
+            | CoreError::QueryGraphBuilderError(QueryGraphBuilderError::QueryParserError(
+                QueryParserError::Structured(se),
+            )) => user_facing_errors::KnownError::new(se).into(),
 
             CoreError::QueryGraphBuilderError(QueryGraphBuilderError::MissingRequiredArgument {
                 argument_name,
@@ -285,7 +297,7 @@ impl From<CoreError> for user_facing_errors::Error {
                 inner_error
             }
 
-            _ => user_facing_errors::Error::from_dyn_error(&err),
+            _ => UnknownError::new(&err).into(),
         }
     }
 }

query-engine/core/src/query_document/error.rs
@@ -1,28 +1,37 @@
 use crate::{schema::InputType, ArgumentValue};
 use fmt::Display;
 use itertools::Itertools;
+use serde::Serialize;
 use std::fmt;
+use user_facing_errors::UserFacingError;
 
 #[derive(Debug)]
-pub struct QueryParserError {
-    pub path: QueryPath,
-    pub error_kind: QueryParserErrorKind,
-}
-
-impl QueryParserError {
-    /// Create a new instance of `QueryParserError`.
-    pub fn new(path: QueryPath, error_kind: QueryParserErrorKind) -> Self {
-        Self { path, error_kind }
-    }
+pub enum QueryParserError {
+    Structured(StructuredQueryParseError),
+    Legacy {
+        path: QueryPath,
+        error_kind: QueryParserErrorKind,
+    },
 }
 
 impl fmt::Display for QueryParserError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "Query parsing/validation error at `{}`: {}",
-            self.path, self.error_kind,
-        )
+        match self {
+            Self::Legacy { path, error_kind } => {
+                write!(f, "Query parsing/validation error at `{}`: {}", path, error_kind,)
+            }
+            Self::Structured(_) => todo!(),
+        }
+    }
+}
+#[derive(Debug, Serialize, Clone)]
+pub struct StructuredQueryParseError {}
+
+impl UserFacingError for StructuredQueryParseError {
+    const ERROR_CODE: &'static str = "P2009";
+
+    fn message(&self) -> String {
+        todo!()
     }
 }
 
@@ -167,7 +176,7 @@ impl Display for FieldCountError {
 
 impl From<prisma_models::DomainError> for QueryParserError {
     fn from(err: prisma_models::DomainError) -> Self {
-        QueryParserError {
+        QueryParserError::Legacy {
             path: QueryPath::default(),
             error_kind: QueryParserErrorKind::AssertionError(format!("Domain error occurred: {err}")),
         }

migration-engine/connectors/migration-connector/src/error.rs
@@ -188,7 +188,7 @@ impl ConnectorError {
     pub fn to_user_facing(&self) -> user_facing_errors::Error {
         match &self.0.user_facing_error {
             Some(known_error) => known_error.clone().into(),
-            None => user_facing_errors::Error::from_dyn_error(self),
+            None => user_facing_errors::UnknownError::new(self).into(),
         }
     }
 

query-engine/request-handlers/src/error.rs
@@ -1,7 +1,6 @@
-use graphql_parser::query::ParseError;
 use query_core::CoreError;
 use thiserror::Error;
-use user_facing_errors::KnownError;
+use user_facing_errors::{KnownError, UnknownError};
 
 #[derive(Debug, Error)]
 #[allow(clippy::large_enum_variant)]
@@ -43,17 +42,6 @@ impl HandlerError {
     pub fn value_fit(details: impl ToString) -> Self {
         Self::ValueFitError(details.to_string())
     }
-
-    pub fn as_known_error(&self) -> Option<KnownError> {
-        match self {
-            HandlerError::ValueFitError(details) => {
-                Some(KnownError::new(user_facing_errors::query_engine::ValueFitError {
-                    details: details.clone(),
-                }))
-            }
-            _ => None,
-        }
-    }
 }
 
 impl From<url::ParseError> for HandlerError {
@@ -68,8 +56,22 @@ impl From<connection_string::Error> for HandlerError {
     }
 }
 
-impl From<ParseError> for HandlerError {
-    fn from(e: ParseError) -> Self {
+impl From<graphql_parser::query::ParseError> for HandlerError {
+    fn from(e: graphql_parser::query::ParseError) -> Self {
         Self::configuration(format!("Error parsing GraphQL query: {e}"))
+    }
+}
+
+impl From<HandlerError> for user_facing_errors::Error {
+    fn from(err: HandlerError) -> Self {
+        match err {
+            HandlerError::Core(ce) => user_facing_errors::Error::from(ce),
+
+            HandlerError::ValueFitError(details) => {
+                KnownError::new(user_facing_errors::query_engine::ValueFitError { details }).into()
+            }
+
+            _ => UnknownError::new(&err).into(),
+        }
     }
 }

.gitignore
@@ -6,6 +6,7 @@ current_connector
 prisma-gpg-private.asc
 .test_config
 *.pending-snap
+.pending.md
 
 *.class
 *.log

introspection-engine/core/src/error_rendering.rs
@@ -3,6 +3,7 @@ use introspection_connector::ConnectorError;
 use jsonrpc_core::types::Error as JsonRpcError;
 use user_facing_errors::{
     common::SchemaParserError, introspection_engine::IntrospectionResultEmpty, Error as UserFacingError, KnownError,
+    UnknownError,
 };
 
 pub fn render_error(crate_error: Error) -> UserFacingError {
@@ -13,7 +14,7 @@ pub fn render_error(crate_error: Error) -> UserFacingError {
         }) => user_facing_error.into(),
         Error::IntrospectionResultEmpty => KnownError::new(IntrospectionResultEmpty).into(),
         Error::DatamodelError(full_error) => KnownError::new(SchemaParserError { full_error }).into(),
-        _ => UserFacingError::from_dyn_error(&crate_error),
+        _ => UnknownError::new(&crate_error).into(),
     }
 }
 

libs/user-facing-errors/src/lib.rs
@@ -63,6 +63,15 @@ pub struct UnknownError {
     pub backtrace: Option<String>,
 }
 
+impl UnknownError {
+    pub fn new(err: &dyn std::error::Error) -> Self {
+        UnknownError {
+            message: err.to_string(),
+            backtrace: None,
+        }
+    }
+}
+
 #[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 pub struct Error {
     is_panic: bool,
@@ -105,17 +114,6 @@ impl Error {
             inner: ErrorType::Unknown(UnknownError {
                 message,
                 backtrace: Some(format!("{:?}", backtrace::Backtrace::new())),
-            }),
-            is_panic: false,
-            batch_request_idx: None,
-        }
-    }
-
-    pub fn from_dyn_error(err: &dyn std::error::Error) -> Self {
-        Error {
-            inner: ErrorType::Unknown(UnknownError {
-                message: err.to_string(),
-                backtrace: None,
             }),
             is_panic: false,
             batch_request_idx: None,

libs/user-facing-errors/src/query_engine/mod.rs
@@ -1,6 +1,9 @@
+mod validation;
+
 use serde::Serialize;
 use std::fmt;
 use user_facing_error_macros::*;
+pub use validation::*;
 
 #[derive(Debug, PartialEq, Eq, Serialize, Clone)]
 #[serde(untagged)]
@@ -126,19 +129,6 @@ pub struct TypeMismatchInvalidCustomType {
 pub struct QueryParsingFailed {
     /// Error(s) encountered when trying to parse a query in the query engine
     pub query_parsing_error: String,
-
-    /// Location of the incorrect parsing, validation in a query. Represented by tuple or object with (line, character)
-    pub query_position: String,
-}
-
-#[derive(Debug, UserFacingError, Serialize)]
-#[user_facing(
-    code = "P2009",
-    message = "Failed to validate the query: `{query_validation_error}` at `{query_position}`"
-)]
-pub struct QueryValidationFailed {
-    /// Error(s) encountered when trying to validate a query in the query engine
-    pub query_validation_error: String,
 
     /// Location of the incorrect parsing, validation in a query. Represented by tuple or object with (line, character)
     pub query_position: String,

commit 7340787387ab1a8d0944ef7621dfaef1094e9116

Fix clippy errors introduced in 066eebeebbe9d8704466a094f1994403117cb101

query-engine/core/src/error.rs
@@ -173,19 +173,18 @@ impl From<CoreError> for user_facing_errors::Error {
                 path,
                 error_kind,
             })) => {
-                let known_error = match error_kind {
-                    QueryParserErrorKind::RequiredValueNotSetError => {
-                        user_facing_errors::KnownError::new(user_facing_errors::query_engine::MissingRequiredValue {
-                            path: format!("{}", path),
-                        })
-                    }
-                    _ => user_facing_errors::KnownError::new(
-                        user_facing_errors::query_engine::LegacyQueryValidationFailed {
-                            query_validation_error: format!("{}", error_kind),
-                            query_position: format!("{}", path),
-                        },
-                    ),
-                };
+                let known_error =
+                    match error_kind {
+                        QueryParserErrorKind::RequiredValueNotSetError => user_facing_errors::KnownError::new(
+                            user_facing_errors::query_engine::MissingRequiredValue { path: path.to_string() },
+                        ),
+                        _ => user_facing_errors::KnownError::new(
+                            user_facing_errors::query_engine::LegacyQueryValidationFailed {
+                                query_validation_error: error_kind.to_string(),
+                                query_position: path.to_string(),
+                            },
+                        ),
+                    };
 
                 known_error.into()
             }

query-engine/core/src/query_document/error.rs
@@ -18,7 +18,7 @@ impl fmt::Display for QueryParserError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Self::Legacy { path, error_kind } => {
-                write!(f, "Query parsing/validation error at `{}`: {}", path, error_kind,)
+                write!(f, "Query parsing/validation error at `{path}`: {error_kind}")
             }
             Self::Structured(_) => todo!(),
         }