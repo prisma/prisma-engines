prisma/prisma-engines

commit a651a3274a6dd554823daff4f94fccb1c99b7ac9

qe: make schema building lazy (#3977)
* qe: make query schema building lazy

In the query engine, incoming queries are validated against a `QuerySchema`
derived from a Prisma schema and a few additional inputs (the flag that turns
on raw queries and extra enabled preview features).

Until this commit, the `QuerySchema` is computed eagerly on query engine
initialization: every allowed input and output type is computed and allocated
in a large object tree that gets larger with larger Prisma schemas. We measured
the impact of `schema-builder` (the component that constructs `QuerySchema`s)
on engine start up time in our effort to improve cold start times of
applications using Prisma, and this, it turns out, can be slow. To the point of
making a significant impact on cold starts for applications with large Prisma
schemas.

The object of this commit is to shift the bulk of that work from query engine
initialization time to query validation time.

Concretely, instead of computing all input and output types up front, we only
compute those of the root (`Query` and `Mutation`) fields on engine
initialization, then compute the allowed inputs and outputs based on the Prisma
schema on a per-query basis. This is achieved mainly by making the input and
output object types' fields lazy thunks. We could have converted
InputObjectType and OutputObjectType into traits for a likely cleaner and more
scalable approach, but that would have been an even larger refactoring.

This commit also includes a number of more minor changes (e.g. the types of
identifiers, and fewer calls to `ObjectType::find_field()`) in order to make
the performance tradeoff work: if we want to build parts of the query schema at
query validation time, we want that operation to be fast, in order for query
latency not to suffer. This brought query validation time to consistently below
1ms on my machine, based on the query validation benchmarks.

**Caution**

query-engine/schema/src/build/mutations/create_one.rs
@@ -1,23 +1,26 @@
 use super::*;
 use crate::{
-    Identifier, IdentifierType, InputField, InputObjectTypeId, InputType, OutputField, OutputType, QueryInfo, QueryTag,
+    Identifier, IdentifierType, InputField, InputObjectType, InputType, OutputField, OutputType, QueryInfo, QueryTag,
 };
 use constants::*;
 use input_types::fields::data_input_mapper::*;
 use output_types::objects;
-use prisma_models::{ModelRef, RelationFieldRef};
+use prisma_models::{Model, RelationFieldRef};
 
 /// Builds a create mutation field (e.g. createUser) for given model.
-pub(crate) fn create_one(ctx: &mut BuilderContext<'_>, model: &ModelRef) -> OutputField {
-    let args = create_one_arguments(ctx, model).unwrap_or_default();
+pub(crate) fn create_one(ctx: &'_ QuerySchema, model: Model) -> OutputField<'_> {
     let field_name = format!("createOne{}", model.name());
+    let cloned_model = model.clone();
+    let model_id = model.id;
 
     field(
         field_name,
-        args,
-        OutputType::object(objects::model::map_type(ctx, model)),
+        Some(Arc::new(move || {
+            create_one_arguments(ctx, model.clone()).unwrap_or_default()
+        })),
+        OutputType::object(objects::model::model_object_type(ctx, cloned_model)),
         Some(QueryInfo {
-            model: Some(model.clone()),
+            model: Some(model_id),
             tag: QueryTag::CreateOne,
         }),
     )
@@ -25,34 +28,20 @@ pub(crate) fn create_one(ctx: &mut BuilderContext<'_>, model: &ModelRef) -> Outp
 
 /// Builds "data" argument intended for the create field.
 /// The data argument is not present if no data can be created.
-pub(crate) fn create_one_arguments(ctx: &mut BuilderContext<'_>, model: &ModelRef) -> Option<Vec<InputField>> {
+pub(crate) fn create_one_arguments(ctx: &'_ QuerySchema, model: Model) -> Option<Vec<InputField<'_>>> {
     let create_types = create_one_input_types(ctx, model, None);
-    let any_empty = create_types.iter().any(|typ| typ.is_empty(&ctx.db));
-    let all_empty = create_types.iter().all(|typ| typ.is_empty(&ctx.db));
-
-    if all_empty {
-        None
-    } else {
-        Some(vec![
-            input_field(ctx, args::DATA, create_types, None).optional_if(any_empty)
-        ])
-    }
+    Some(vec![input_field(args::DATA, create_types, None).optional()])
 }
 
 pub(crate) fn create_one_input_types(
-    ctx: &mut BuilderContext<'_>,
-    model: &ModelRef,
-    parent_field: Option<&RelationFieldRef>,
-) -> Vec<InputType> {
-    let checked_input = InputType::object(checked_create_input_type(ctx, model, parent_field));
+    ctx: &'_ QuerySchema,
+    model: Model,
+    parent_field: Option<RelationFieldRef>,
+) -> Vec<InputType<'_>> {
+    let checked_input = InputType::object(checked_create_input_type(ctx, model.clone(), parent_field.clone()));
     let unchecked_input = InputType::object(unchecked_create_input_type(ctx, model, parent_field));
 
-    // If the inputs are equal, only use one.
-    if checked_input == unchecked_input {
-        vec![checked_input]
-    } else {
-        vec![checked_input, unchecked_input]
-    }
+    vec![checked_input, unchecked_input]
 }
 
 /// Builds the create input type (<x>CreateInput / <x>CreateWithout<y>Input)
@@ -60,29 +49,25 @@ pub(crate) fn create_one_input_types(
 /// "Checked" input refers to disallowing writing relation scalars directly, as it can lead to unintended
 /// data integrity violations if used incorrectly.
 fn checked_create_input_type(
-    ctx: &mut BuilderContext<'_>,
-    model: &ModelRef,
-    parent_field: Option<&RelationFieldRef>,
-) -> InputObjectTypeId {
+    ctx: &'_ QuerySchema,
+    model: Model,
+    parent_field: Option<RelationFieldRef>,
+) -> InputObjectType<'_> {
     // We allow creation from both sides of the relation - which would lead to an endless loop of input types
     // if we would allow to create the parent from a child create that is already a nested create.
     // To solve it, we remove the parent relation from the input ("Without<Parent>").
     let ident = Identifier::new_prisma(IdentifierType::CheckedCreateInput(
         model.clone(),
-        parent_field.map(|pf| pf.related_field()),
+        parent_field.as_ref().map(|pf| pf.related_field()),
     ));
 
-    return_cached_input!(ctx, &ident);
-
-    let input_object = init_input_object_type(ident.clone());
-    let id = ctx.cache_input_type(ident, input_object);
-
-    let filtered_fields = filter_checked_create_fields(model, parent_field);
-    let field_mapper = CreateDataInputFieldMapper::new_checked();
-    let input_fields = field_mapper.map_all(ctx, &filtered_fields);
-
-    ctx.db[id].set_fields(input_fields);
-    id
+    let mut input_object = init_input_object_type(ident);
+    input_object.fields = Arc::new(move || {
+        let mut filtered_fields = filter_checked_create_fields(&model, parent_field.clone());
+        let field_mapper = CreateDataInputFieldMapper::new_checked();
+        field_mapper.map_all(ctx, &mut filtered_fields)
+    });
+    input_object
 }
 
 /// Builds the create input type (<x>UncheckedCreateInput / <x>UncheckedCreateWithout<y>Input)
@@ -90,34 +75,33 @@ fn checked_create_input_type(
 /// "Unchecked" input refers to allowing to write _all_ scalars on a model directly, which can
 /// lead to unintended data integrity violations if used incorrectly.
 fn unchecked_create_input_type(
-    ctx: &mut BuilderContext<'_>,
-    model: &ModelRef,
-    parent_field: Option<&RelationFieldRef>,
-) -> InputObjectTypeId {
+    ctx: &'_ QuerySchema,
+    model: Model,
+    parent_field: Option<RelationFieldRef>,
+) -> InputObjectType<'_> {
     // We allow creation from both sides of the relation - which would lead to an endless loop of input types
     // if we would allow to create the parent from a child create that is already a nested create.
     // To solve it, we remove the parent relation from the input ("Without<Parent>").
     let ident = Identifier::new_prisma(IdentifierType::UncheckedCreateInput(
         model.clone(),
-        parent_field.map(|pf| pf.related_field()),
+        parent_field.as_ref().map(|pf| pf.related_field()),
     ));
 
-    return_cached_input!(ctx, &ident);
-
-    let input_object = init_input_object_type(ident.clone());
-    let id = ctx.cache_input_type(ident, input_object);
-
-    let filtered_fields = filter_unchecked_create_fields(model, parent_field);
-    let field_mapper = CreateDataInputFieldMapper::new_unchecked();
-    let input_fields = field_mapper.map_all(ctx, &filtered_fields);
-
-    ctx.db[id].set_fields(input_fields);
-    id
+    let mut input_object = init_input_object_type(ident);
+    input_object.fields = Arc::new(move || {
+        let mut filtered_fields = filter_unchecked_create_fields(&model, parent_field.as_ref());
+        let field_mapper = CreateDataInputFieldMapper::new_unchecked();
+        field_mapper.map_all(ctx, &mut filtered_fields)
+    });
+    input_object
 }
 
 /// Filters the given model's fields down to the allowed ones for checked create.
-fn filter_checked_create_fields(model: &ModelRef, parent_field: Option<&RelationFieldRef>) -> Vec<ModelField> {
-    model.fields().filter_all(|field| {
+fn filter_checked_create_fields(
+    model: &Model,
+    parent_field: Option<RelationFieldRef>,
+) -> impl Iterator<Item = ModelField> + '_ {
+    model.fields().filter_all(move |field| {
         match field {
             // Scalars must be writable and not an autogenerated ID, which are disallowed for checked inputs
             // regardless of whether or not the connector supports it.
@@ -126,7 +110,10 @@ fn filter_checked_create_fields(model: &ModelRef, parent_field: Option<&Relation
             // If the relation field `rf` is the one that was traversed to by the parent relation field `parent_field`,
             // then exclude it for checked inputs - this prevents endless nested type circles that are useless to offer as API.
             ModelField::Relation(rf) => {
-                let field_was_traversed_to = parent_field.filter(|pf| pf.related_field().id == rf.id).is_some();
+                let field_was_traversed_to = parent_field
+                    .as_ref()
+                    .filter(|pf| pf.related_field().id == rf.id)
+                    .is_some();
                 !field_was_traversed_to
             }
 
@@ -137,7 +124,10 @@ fn filter_checked_create_fields(model: &ModelRef, parent_field: Option<&Relation
 }
 
 /// Filters the given model's fields down to the allowed ones for unchecked create.
-fn filter_unchecked_create_fields(model: &ModelRef, parent_field: Option<&RelationFieldRef>) -> Vec<ModelField> {
+fn filter_unchecked_create_fields<'a>(
+    model: &'a Model,
+    parent_field: Option<&'a RelationFieldRef>,
+) -> impl Iterator<Item = ModelField> + 'a {
     let linking_fields = if let Some(parent_field) = parent_field {
         let child_field = parent_field.related_field();
         if child_field.is_inlined_on_enclosing_model() {
@@ -152,7 +142,7 @@ fn filter_unchecked_create_fields(model: &ModelRef, parent_field: Option<&Relati
         vec![]
     };
 
-    model.fields().filter_all(|field| match field {
+    model.fields().filter_all(move |field| match field {
         // In principle, all scalars are writable for unchecked inputs. However, it still doesn't make any sense to be able to write the scalars that
         // link the model to the parent record in case of a nested unchecked create, as this would introduce complexities we don't want to deal with right now.
         ModelField::Scalar(sf) => !linking_fields.contains(sf),

commit b9dd3673162677653a43127c6ec2b3c944bcb3e5

qe: fix regression on CreateOne data argument nullability
This is a regression introduced by
https://github.com/prisma/prisma-engines/pull/3977 — we failed to make
the `data` argument required on CreateOne inputs if any field is
required in the model.

query-engine/schema/src/build/mutations/create_one.rs
@@ -26,19 +26,23 @@ pub(crate) fn create_one(ctx: &'_ QuerySchema, model: Model) -> OutputField<'_> 
 
 /// Builds "data" argument intended for the create field.
 /// The data argument is not present if no data can be created.
-pub(crate) fn create_one_arguments(ctx: &'_ QuerySchema, model: Model) -> Option<Vec<InputField<'_>>> {
+pub(crate) fn create_one_arguments(ctx: &QuerySchema, model: Model) -> Option<Vec<InputField<'_>>> {
+    let any_field_required = model
+        .fields()
+        .all()
+        .any(|f| f.is_required() && f.as_scalar().map(|f| f.default_value().is_none()).unwrap_or(true));
     let create_types = create_one_input_types(ctx, model, None);
-    Some(vec![input_field(args::DATA, create_types, None).optional()])
+    let field = input_field(args::DATA, create_types, None);
+    Some(vec![field.optional_if(!any_field_required)])
 }
 
 pub(crate) fn create_one_input_types(
-    ctx: &'_ QuerySchema,
+    ctx: &QuerySchema,
     model: Model,
     parent_field: Option<RelationFieldRef>,
 ) -> Vec<InputType<'_>> {
     let checked_input = InputType::object(checked_create_input_type(ctx, model.clone(), parent_field.clone()));
     let unchecked_input = InputType::object(unchecked_create_input_type(ctx, model, parent_field));
-
     vec![checked_input, unchecked_input]
 }
 

query-engine/core-tests/tests/query_validation_tests/create/schema.prisma
@@ -1,0 +1,36 @@
+datasource db {
+  provider = "postgres"
+  url      = env("SOME_DB")
+}
+
+// / User model comment
+model User {
+  id    String  @id @default(uuid())
+  email String  @unique
+  // / name comment
+  name  String?
+  posts Post[]
+  Like  Like[]
+}
+
+model Post {
+  id        String   @id @default(cuid())
+  createdAt DateTime @default(now())
+  updatedAt DateTime @updatedAt
+  published Boolean
+  title     String
+  content   String?
+  authorId  String?
+  author    User?    @relation(fields: [authorId], references: [id])
+  Like      Like[]
+}
+
+model Like {
+  id     String @id @default(cuid())
+  userId String
+  user   User   @relation(fields: [userId], references: [id])
+  postId String
+  post   Post   @relation(fields: [postId], references: [id])
+
+  @@unique([userId, postId])
+}

query-engine/core-tests/tests/query_validation_tests/create_post_empty.expected.json
@@ -1,0 +1,143 @@
+{
+  "is_panic": false,
+  "message": "`data`: A value is required but not set",
+  "meta": {
+    "kind": "RequiredArgumentMissing",
+    "inputTypes": [
+      {
+        "kind": "object",
+        "name": "PostCreateInput",
+        "fields": [
+          {
+            "name": "id",
+            "typeNames": [
+              "String"
+            ],
+            "required": false
+          },
+          {
+            "name": "createdAt",
+            "typeNames": [
+              "DateTime"
+            ],
+            "required": false
+          },
+          {
+            "name": "updatedAt",
+            "typeNames": [
+              "DateTime"
+            ],
+            "required": false
+          },
+          {
+            "name": "published",
+            "typeNames": [
+              "Boolean"
+            ],
+            "required": true
+          },
+          {
+            "name": "title",
+            "typeNames": [
+              "String"
+            ],
+            "required": true
+          },
+          {
+            "name": "content",
+            "typeNames": [
+              "String",
+              "Null"
+            ],
+            "required": false
+          },
+          {
+            "name": "author",
+            "typeNames": [
+              "UserCreateNestedOneWithoutPostsInput"
+            ],
+            "required": false
+          },
+          {
+            "name": "Like",
+            "typeNames": [
+              "LikeCreateNestedManyWithoutPostInput"
+            ],
+            "required": false
+          }
+        ]
+      },
+      {
+        "kind": "object",
+        "name": "PostUncheckedCreateInput",
+        "fields": [
+          {
+            "name": "id",
+            "typeNames": [
+              "String"
+            ],
+            "required": false
+          },
+          {
+            "name": "createdAt",
+            "typeNames": [
+              "DateTime"
+            ],
+            "required": false
+          },
+          {
+            "name": "updatedAt",
+            "typeNames": [
+              "DateTime"
+            ],
+            "required": false
+          },
+          {
+            "name": "published",
+            "typeNames": [
+              "Boolean"
+            ],
+            "required": true
+          },
+          {
+            "name": "title",
+            "typeNames": [
+              "String"
+            ],
+            "required": true
+          },
+          {
+            "name": "content",
+            "typeNames": [
+              "String",
+              "Null"
+            ],
+            "required": false
+          },
+          {
+            "name": "authorId",
+            "typeNames": [
+              "String",
+              "Null"
+            ],
+            "required": false
+          },
+          {
+            "name": "Like",
+            "typeNames": [
+              "LikeUncheckedCreateNestedManyWithoutPostInput"
+            ],
+            "required": false
+          }
+        ]
+      }
+    ],
+    "argumentPath": [
+      "data"
+    ],
+    "selectionPath": [
+      "createOnePost"
+    ]
+  },
+  "error_code": "P2012"
+}